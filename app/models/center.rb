476813e17bed928d7799849024b1ba1865054448
Yingqi Chen <chanwk18@hotmail.com>
2019-08-28 19:12:51 +0000

    set up associations

--- app/models/center.rb
+++ app/models/center.rb
@@ -1,2 +1,5 @@
 class Center < ActiveRecord::Base
+
+  has_many :reviews
+  has_many :users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :reviews
+  has_many :centers, through: :reviews
 end
