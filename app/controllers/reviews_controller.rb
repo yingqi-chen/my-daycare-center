db8453b2568b1f742e4007e9afc7dcd39150ee0f
Yingqi Chen <chanwk18@hotmail.com>
2019-08-31 21:44:23 +0000

    add review/new page

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,12 +8,33 @@ class ReviewsController < ApplicationController
 
   # GET: /reviews/new
   get "/reviews/new" do
-    erb :"/reviews/new.html"
+    @centers=Center.all
+    erb :"/reviews/new"
   end
 
   # POST: /reviews
   post "/reviews" do
-    redirect "/reviews"
+    rate=params[:review][:rate].to_i
+    center_id=params[:review][:center_id].to_i
+    if Helper.log_in?(session)
+      if rate>=1 && rate <= 5
+        center=Center.find(center_id)
+        if Helper.current_user(session).centers.include?(center)
+          flash[:error]="You already rated this center before. Here is the review you have."
+          review=Review.find_by :center_id=>center_id
+          redirect to "/reviews/#{review.id}"
+        else
+          @review=Review.new(params[:review])
+          @review.user=Helper.current_user(session)
+          @review.save
+          redirect "/reviews"
+        end
+      else
+        flash[:error]="The rate you input is not a valid number. It should be between 1-5."
+        redirect to "/reviews/new"
+      end
+      redirect to "/login"
+    end
   end
 
   # GET: /reviews/5
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :center
 
-  validates :user_id, presence: true
+  validates :user_id, :center_id, presence: true
   validates :rate, presence: true
   validates_associated :user
 end
